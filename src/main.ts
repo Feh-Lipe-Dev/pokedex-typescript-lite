f68229c9babb18b421de80e755b75df55e0cadec
Felipe Goncalves <felipe.cleia05@gmail.com>
2026-06-07 02:06:48 -0300

    fix: correções na função principal

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { buscarPokemon } from "./services/PokeApiService";
 import { CatalogoPokemon } from "./controllers/TerminalController";
-//import { salvarBox, carregarBox } from "./services/BoxService";
+import { formatarMensagemOk, formatarMensagemAviso } from "./utils/textFormatters";
 
 async function main() {
+    console.log("\n.........................");
+    console.log(" POKEMON TYPESCRIPT LITE");
+    console.log("'''''''''''''''''''''''''");
     
     // Exemplo de uso da função buscarPokemon
+    console.log(" ## Buscando Pokémon por nome ou ID ##");
+    console.log("'''''''''''''''''''''''''''''''''''''''\n");
+
     let nomeOuId = "pikachu";
     const pokemon = await buscarPokemon(nomeOuId);
     console.log(pokemon); // Saída do Pokémon encontrado (objeto PokemonResumo)
     nomeOuId = "9999";
     const pokemonInexistente = await buscarPokemon(nomeOuId);
     console.log(pokemonInexistente); // Saída null, pois o Pokémon não existe
+
+    console.log(" \nCATÁLOGO DE POKÉMON");
+    console.log("''''''''''''''''''''''");
 
     // Exemplo de uso da classe CatalogoPokemon
     const catalogo = new CatalogoPokemon();
@@ -51,6 +60,32 @@ async function main() {
     // Listar novamente para verificar a remoção
     catalogo.listar();
 
+    // buscar um Pokémon por ID
+    const pokemonBuscado = catalogo.buscarPorId(4); // Buscar o Charmander (ID 4)
+    console.log("Buscando Pokémon por ID...");
+    if (pokemonBuscado) {
+        console.log(formatarMensagemOk(`Pokémon encontrado: ${pokemonBuscado.nome}`));
+    } else {
+        console.log(formatarMensagemAviso("Nenhum Pokémon encontrado com esse ID."));
+    }
+
+    // Verificar se todos os Pokémon têm nome
+    const todosTemNome = catalogo.todosTêmNome();
+    console.log("\nVerificando se todos os Pokémon têm nome...");
+    console.log(todosTemNome ? formatarMensagemOk("Todos os Pokémon têm nome.") : formatarMensagemAviso("Nem todos os Pokémon têm nome."));
+
+    // Calcular o peso total dos Pokémon no catálogo
+    const pesoTotal = catalogo.calcularPesoTotal();
+    console.log("\nCalculando o peso total dos Pokémon no catálogo...");
+    console.log(formatarMensagemOk(`Peso total dos Pokémon: ${pesoTotal} kg`));
+
+    // Obter todos os Pokémon do catálogo
+    const todosPokemons = catalogo.obterTodos();
+    console.log("\nObtendo todos os Pokémon do catálogo...");
+    console.log(todosPokemons);
+
+    // Exibir estatísticas do catálogo
+    catalogo.exibirEstatisticas();
 }
 
 main();     
